8431614f4d85b6901386fa4b5c462b5892559e2d
Yan Rudenko <yan.rudenko@linkdigital.com.au>
2022-05-23 12:30:02 +0300

    COR-49 / Fix Publishing

--- contact/templatetags/modal_tags.py
+++ contact/templatetags/modal_tags.py
@@ -5,16 +5,17 @@ register = template.Library()
 
 @register.inclusion_tag('tags/form_modal.html', takes_context=True)
 def form_modal(context):
-    request = context['request']
-    settings = CkanOrgSettings.for_request(request)
-    form_page = settings.modal_form_page
+    if 'request' in context:
+        request = context['request']
+        settings = CkanOrgSettings.for_request(request)
+        form_page = settings.modal_form_page
 
-    if not form_page:
-        return context
+        if not form_page:
+            return context
 
-    form_page = form_page.specific
+        form_page = form_page.specific
 
-    context['form_page'] = form_page
-    context['form'] = form_page.get_form(page=form_page, user=request.user)
+        context['form_page'] = form_page
+        context['form'] = form_page.get_form(page=form_page, user=request.user)
 
     return context
