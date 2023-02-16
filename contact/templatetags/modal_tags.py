5978bdb4ff7c50789521f3e33943ac99870bfcbc
alexmorev <aleksey.morev@linkdigital.com.au>
2023-02-16 13:05:39 +0200

    COR-69 / fix contact buttons visibility

--- contact/templatetags/modal_tags.py
+++ contact/templatetags/modal_tags.py
@@ -4,12 +4,17 @@ from ..models import CkanOrgSettings, ContactPage
 register = template.Library()
 
 @register.inclusion_tag('tags/form_modal.html', takes_context=True)
-def form_modal(context, form_name="Contact Form"):
+def form_modal(context, form_name=None):
     if 'request' in context:
         request = context['request']
-        form_page = ContactPage.objects.get(form_name = form_name)
-        if not form_page:
+        if form_name == None:
+            settings = CkanOrgSettings.for_request(request)
+            form_page = settings.modal_form_page
+            if not form_page:
                 return context
+            form_page = form_page.specific
+        else:
+            form_page = ContactPage.objects.get(form_name = form_name)
         context['form_page'] = form_page
         context['form'] = form_page.get_form(page=form_page, user=request.user)
 
