0578a032a67266d5d4ae5e411767cb85d94937d2
alexmorev <aleksey.morev@linkdigital.com.au>
2023-04-28 01:38:07 +0300

    COR-75 / add search bar to the blog page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import UsersBlogPostListView, TagsBlogPostListView, CategoriesBlogPostListView
+from .views import (
+    UsersBlogPostListView,
+    TagsBlogPostListView,
+    CategoriesBlogPostListView,
+    SearchBlogPostListView
+)
 
 
 urlpatterns = [
     path('author/<username>', UsersBlogPostListView.as_view(), name='user_post_list'),
     path('tag/<tag>', TagsBlogPostListView.as_view(), name='tag_post_list'),
     path('category/<cat_id>', CategoriesBlogPostListView.as_view(), name='category_post_list'),
+    path('search', SearchBlogPostListView.as_view(), name='search_post_list'),
 ]
