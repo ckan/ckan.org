f68ffdf8f3550a4ae34d321050f712ead2f98f6a
agmorev <agmorev@gmail.com>
2024-02-27 21:42:43 +0200

    COR-107 / code format

--- blog/blocks.py
+++ blog/blocks.py
@@ -1,14 +1,21 @@
-from django.db import models
 from wagtail import blocks
 from wagtail.images.blocks import ImageChooserBlock
 
 
 class ImageWithCaption(blocks.StructBlock):
 
-    image = ImageChooserBlock(label="Image", required=True, help_text="Add an Image")
-    caption = blocks.CharBlock(label="Caption", required=True, help_text="Provide a caption for the image")
+    image = ImageChooserBlock(
+        label="Image",
+        required=True,
+        help_text="Add an image expanded to fill the entire screen width"
+    )
+    caption = blocks.CharBlock(
+        label="Caption",
+        required=False,
+        help_text="Provide a caption for the image"
+    )
 
     class Meta:
-        template = 'blog/blocks/imagewithcaption.html'
-        form_classname = 'imagewithcaption'
-        icon = 'picture'
+        template = "blog/blocks/imagewithcaption.html"
+        form_classname = "imagewithcaption"
+        icon = "image"
--- events/templatetags/events_tags.py
+++ events/templatetags/events_tags.py
@@ -1,5 +1,6 @@
 from django import template
 
+
 register = template.Library()
 
 
@@ -7,7 +8,7 @@ register = template.Library()
 def event_start(start_date):
     return start_date.strftime("%d %B %Y - %H:%M")
 
+
 @register.simple_tag()
 def event_start_date(start_date):
     return start_date.strftime("%d %B %Y")
-
