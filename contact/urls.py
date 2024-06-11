27aa479c565ec4ba45b1f643120867addb7906b4
agmorev <agmorev@gmail.com>
2024-06-11 13:22:39 +0300

    COR-111 / confirmation message after contact form submission

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import activate_subscription, SubscriptionPage
 
 
 urlpatterns = [
-    path('subscription/', SubscriptionPage.as_view(), name='subscription_page'),
+    re_path(r'subscription/?$', SubscriptionPage.as_view(), name='subscription_page'),
     path('subscription/activate/<eidb64>/<token>', activate_subscription, name='subscription_activate'), 
 ]
