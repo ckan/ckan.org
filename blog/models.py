22ddb4e744f3841d1c17f64ec3cfd469df09a214
alexmorev <aleksey.morev@linkdigital.com.au>
2024-07-29 01:45:38 +0300

    COR-115 / various improvements

--- blog/models.py
+++ blog/models.py
@@ -314,6 +314,7 @@ class BlogPostPage(MetadataPageMixin, Page):
         index.AutocompleteField("post_subtitle"),
         index.SearchField("author"),
         index.AutocompleteField("author"),
+        index.FilterField('last_published_at'),
     ]
 
     promote_panels = [
