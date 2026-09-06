8f103e176446e8cd315178f36e99063f4e7c2614
Aleksey Morev <aleksey.morev@linkdigital.com.au>
2026-09-06 19:16:59 +0000

    COR-170 / add logs

--- contact/templatetags/modal_tags.py
+++ contact/templatetags/modal_tags.py
@@ -1,3 +1,4 @@
+import logging
 import uuid
 
 from django import template
@@ -7,17 +8,21 @@ from ..models import CkanOrgSettings, ContactPage
 
 register = template.Library()
 
+logger = logging.getLogger(__name__)
+
 
 @register.inclusion_tag("tags/form_modal.html", takes_context=True)
 def form_modal(context, form_name=None):
     request = context.get("request")
     if request is None:
+        logger.debug("form_modal: no request in context; skipping")
         return context
 
     if form_name is None:
         settings = CkanOrgSettings.for_request(request)
         form_page = settings.modal_form_page
         if not form_page:
+            logger.debug("form_modal: no modal form page configured; skipping")
             return context
         form_page = form_page.specific
     else:
@@ -26,6 +31,7 @@ def form_modal(context, form_name=None):
         # DoesNotExist/MultipleObjectsReturned and 500-ing the whole page.
         form_page = ContactPage.objects.filter(form_name=form_name).first()
         if form_page is None:
+            logger.debug("form_modal: no form page found for form_name '%s'; skipping", form_name)
             return context
 
     context["form_page"] = form_page
